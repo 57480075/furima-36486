5f27aab691819587011624088a21d9dfa448862c
57480075 <ishidakunpixarsoul@gmail.com>
2021-11-30 09:17:15 +0900

    バリデーションの記述

--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -1,4 +1,9 @@
 class Purchase < ApplicationRecord
+  with_options null: false, foreign_key: true do
+    validates :user
+    validates :item
+  end
+
   belongs_to :user
   has_one :shipping_address
   belongs_to :user
--- app/models/shipping_address.rb
+++ app/models/shipping_address.rb
@@ -1,3 +1,17 @@
 class ShippingAddress < ApplicationRecord
+  with_options presence: true do
+    validates :zip_code
+    validates :state_id
+    validates :city
+    validates :address_1
+    validates :phone_number
+    validates :purchase, foreign_key: true
+  end
+
+  validates :address_2
+
   belongs_to :purchase
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :state
 end
